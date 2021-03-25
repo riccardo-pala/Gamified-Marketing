84c2b7448fc516586b7110cd3e256266df640b09
Riccardo Pala <riccardo.pala98@gmail.com>
2021-03-25 18:09:03 +0100

    user

--- GamifiedMarketingEJB/ejbModule/entities/User.java
+++ GamifiedMarketingEJB/ejbModule/entities/User.java
@@ -13,19 +13,21 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 
 @Entity
 @Table(name = "users", schema = "gmdb")
 @NamedQuery(name = "User.checkCredentials", query = "SELECT r FROM User r  WHERE r.username = ?1 and r.password = ?2")
+@SequenceGenerator(name="seq", initialValue=1, allocationSize=1)
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	
 	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
 	private int id;
 	
 	private String firstname;
@@ -59,6 +61,7 @@ public class User implements Serializable {
 
 	public User(String firstname, String lastname, String username, String email,
 			String password, Boolean isBanned, Boolean isAdmin, int totalPoints) {
+		
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.username = username;
