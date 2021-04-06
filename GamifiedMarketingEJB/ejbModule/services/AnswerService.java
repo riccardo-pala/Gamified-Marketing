4b60830f4064a07698869c8f7ea9aa3753510f54
alessandroimmordino <aleimmordino@gmail.com>
2021-04-06 12:42:38 +0200

    z

--- GamifiedMarketingEJB/ejbModule/services/AnswerService.java
+++ GamifiedMarketingEJB/ejbModule/services/AnswerService.java
@@ -1,8 +1,12 @@
 package services;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import entities.Answer;
 
 @Stateless
 public class AnswerService {
@@ -12,6 +16,12 @@ public class AnswerService {
 	
 	
 	public AnswerService() {
+	}
+	
+	public List<Answer> getAnswersByUserAndQuestionnaire(int userId, int questionnaireId){
+		List<Answer> answers = em.createQuery("SELECT a FROM Answer a WHERE a.questionnaire.id=?1 AND a.user.id=?2")
+								.setParameter(1, questionnaireId).setParameter(2, userId).getResultList();
+		return answers;
 	}
 	
 
